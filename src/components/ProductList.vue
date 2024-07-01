85ee7ddcdd10758c7ba579957e2cebb824b5d715
Jing Han, Huang <7jesstc1999@gmail.com>
2024-07-01 15:59:16 +0800

    feat: add ProductList component

---
+++ src/components/ProductList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h2>商品列表</h2>
+      <div class="row">
+        <div class="col-md-4" v-for="product in products" :key="product.id">
+          <div class="card mb-4">
+            <div class="card-body">
+              <h5 class="card-title">{{ product.name }}</h5>
+              <p class="card-text">{{ product.price }} 元</p>
+              <button class="btn btn-primary" @click="addToCart(product)">加入購物車</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  // import { ref } from 'vue';
+  
+  export default {
+    props: {
+      products: Array,
+      addToCart: Function,
+    },
+  };
+  </script>
