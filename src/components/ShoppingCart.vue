a599c2849a06ebc9b62b15651a54b4ecb5f9e816
Jing Han, Huang <7jesstc1999@gmail.com>
2024-07-02 21:06:13 +0800

    feat: change the layout of item count

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -3,11 +3,18 @@
       <h2>購物車</h2>
       <ul class="list-group">
         <li v-for="item in cart" :key="item.product.id" class="list-group-item d-flex justify-content-between align-items-center">
-          <span>{{ item.product.name }} x {{ item.quantity }}</span>
-          <span>
-            <button class="btn btn-sm btn-secondary" @click="decreaseQuantity(item)">-</button>
-            <button class="btn btn-sm btn-secondary" @click="increaseQuantity(item)">+</button>
-            <button class="btn btn-sm btn-danger" @click="removeFromCart(item.product.id)">移除</button>
+          <span class="me-auto p-2 align-self-center">{{ item.product.name }}</span>
+          <span class="p-2 align-self-center">
+            <span class="input-group mb-3">
+              <button class="btn btn-sm btn-secondary" @click="decreaseQuantity(item)">-</button>
+              <span class="input-group-text">{{ item.quantity }}</span>
+              <button class="btn btn-sm btn-secondary" @click="increaseQuantity(item)">+</button>
+            </span>
+          </span>
+          <span class="p-2 align-self-center">
+            <button class="btn btn-sm btn-danger" @click="removeFromCart(item.product.id)">
+              <i class="bi bi-trash"></i>
+            </button>
           </span>
         </li>
       </ul>
