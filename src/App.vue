ac139863b82f7b303dec2d66d4c0c68015800cb1
Jing Han, Huang <7jesstc1999@gmail.com>
2024-07-05 01:32:46 +0800

    feat: add Items to shopping cart

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,17 @@
     <h1 class="my-4">Vue 3 商店</h1>
     <div class="row">
       <div class="col-md-8">
-        <ProductList :products="products" :addToCart="addToCart" />
+        <ProductList
+          :products="products"
+          :addToCart="addToCart"
+          @ProductNumChange="handleProductNumChange"
+        />
       </div>
       <div class="col-md-4">
         <ShoppingCart 
           :cart="cart"
-          :increaseQuantity="increaseQuantity"
-          :decreaseQuantity="decreaseQuantity"
           :removeFromCart="removeFromCart"
+          @CartNumChange="handleCartNumChange"
         />
       </div>
     </div>
@@ -18,7 +21,7 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 import ProductList from './components/ProductList.vue';
 import ShoppingCart from './components/ShoppingCart.vue';
 
@@ -34,40 +37,40 @@ export default {
       { id: 3, name: '商品3', price: 300 },
     ]);
 
-    const cart = ref([]);
+    const cart = reactive([]);
+    const current_count = reactive([]);
 
     const addToCart = (product) => {
-      const cartItem = cart.value.find(item => item.product.id === product.id);
+      const cartItem = cart.find(item => item.product.id === product.id);
+      current_count[product.id] = current_count[product.id] ? current_count[product.id] : 1;
+
       if (cartItem) {
-        cartItem.quantity++;
+        cartItem.quantity += current_count[product.id];
       } else {
-        cart.value.push({ product, quantity: 1 });
-      }
-    };
-
-    const increaseQuantity = (item) => {
-      item.quantity++;
-    };
-
-    const decreaseQuantity = (item) => {
-      if (item.quantity > 1) {
-        item.quantity--;
-      } else {
-        cart.value = cart.value.filter(i => i.product.id !== item.product.id);
+        cart.push({ product, quantity: current_count[product.id] });
       }
     };
 
     const removeFromCart = (productId) => {
-      cart.value = cart.value.filter(item => item.product.id !== productId);
+      cart.values = cart.filter(item => item.product.id !== productId);
+    };
+
+    const handleProductNumChange = (updatedCount) => {
+      current_count[updatedCount['id']] = updatedCount['newNum'];
+    };
+
+    const handleCartNumChange = (updatedCount) => {
+      const updatedProduct = cart.find(item => item.product.id === updatedCount['id']);
+      updatedProduct.quantity = updatedCount['newNum'];
     };
 
     return {
       products,
       cart,
       addToCart,
-      increaseQuantity,
-      decreaseQuantity,
       removeFromCart,
+      handleProductNumChange,
+      handleCartNumChange,
     };
   }
 };
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -5,9 +5,8 @@
         <li v-for="item in cart" :key="item.product.id" class="list-group-item d-flex justify-content-between align-items-center">
           <span class="me-auto p-2 align-self-center">{{ item.product.name }}</span>
           <CounterBtn 
-            :item="item"
-            :increaseQuantity="increaseQuantity"
-            :decreaseQuantity="decreaseQuantity"
+            @ChangeNum="changeNum(item.product.id, $event)" 
+            :productNum=item.quantity 
           />
           <span class="p-2 align-self-center">
             <button class="btn btn-sm btn-danger" @click="removeFromCart(item.product.id)">
@@ -21,16 +20,25 @@
   
   <script>
   import CounterBtn from './CounterBtn.vue';
+  import { defineComponent } from 'vue';
   
-  export default {
+  export default defineComponent({
     components: {
       CounterBtn,
     },
     props: {
       cart: Array,
-      increaseQuantity: Function,
-      decreaseQuantity: Function,
       removeFromCart: Function,
     },
-  };
+    setup (props, { emit }) {
+
+      const changeNum = (id, newNum) => {
+        emit("CartNumChange", { id, newNum })
+      }
+
+      return {
+        changeNum,
+      }
+    },
+  });
   </script>
