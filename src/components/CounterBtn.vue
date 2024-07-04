d7eb1b66601f6bad4e0c874721a501a149c6ba2a
Jing Han, Huang <7jesstc1999@gmail.com>
2024-07-04 18:15:06 +0800

    feat: CounterBtn can change quantity independently

--- src/components/CounterBtn.vue
+++ src/components/CounterBtn.vue
@@ -1,21 +1,39 @@
 <template>
     <div class="p-2 align-self-center">
         <span class="input-group mb-3">
-            <button class="btn btn-sm btn-secondary" @click="increaseQuantity(item)">-</button>
-            <span class="input-group-text">{{ item.quantity }}</span>
-            <button class="btn btn-sm btn-secondary" @click="increaseQuantity(item)">+</button>
+            <button class="btn btn-sm btn-secondary" @click="changeQantity(-1)">-</button>
+            <span class="input-group-text">{{ localProductNum }}</span>
+            <button class="btn btn-sm btn-secondary" @click="changeQantity(1)">+</button>
         </span>
     </div>
 </template>
 
 <script>
+import { defineComponent, toRefs, ref, watch } from 'vue';
   
-export default {
+export default defineComponent({
     props: {
-        item: Object,
-        increaseQuantity: Function,
-        decreaseQuantity: Function,
+        productNum: Number,
     },
-};
+    setup(props, { emit }) {
+        const { productNum } = toRefs(props);
+        const localProductNum = ref(productNum.value);
+
+        const changeQantity = (increase_decrease) => {
+            (localProductNum.value > 0 || increase_decrease > 0) && 
+                (localProductNum.value += increase_decrease);
+            emit("ChangeNum", localProductNum.value);
+        };
+
+        watch(productNum, (newVal) => {
+            localProductNum.value = newVal;
+        });
+
+        return {
+            localProductNum,
+            changeQantity,
+        }
+    }
+});
 
 </script>
